983a3efcee5e2490d2b11c51e2e13be70b946fb5
Vishnu <shendgevishnu308@gmail.com>
2024-10-21 10:43:44 +0530

    changed the project

--- Pharmacist_insert.sql
+++
@@ -1,4 +0,0 @@
--- Inserting sample data into Pharmacist Table
-INSERT INTO pharmacist VALUES (1, 'Anita', 'Deshmukh', 9876543210, 'anita.deshmukh@example.com', '10, FC Road, Pune, Maharashtra', 'PH12345');
-INSERT INTO pharmacist VALUES (2, 'Rahul', 'Kumar', 9123456789, 'rahul.kumar@example.com', '20, Jangli Maharaj Road, Pune, Maharashtra', 'PH54321');
-INSERT INTO pharmacist VALUES (3, 'Sanjay', 'Rao', 9988776655, 'sanjay.rao@example.com', '30, Sadashiv Peth, Pune, Maharashtra', 'PH67890');
